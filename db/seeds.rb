f7ca08aba2cba5aecf0c72aa85591283299a2cee
Karl Keller <kgk86@hotmail.com>
2021-01-16 15:03:28 +0100

    Create seeds for Ingredients from JSON list

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Destroy previous ingredients
+Ingredient.destroy_all
+
+# Create seeds
+puts "Fetching JSON list"
+require 'json'
+require 'open-uri'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+user_serialized = open(url).read
+user = JSON.parse(user_serialized)
+
+puts "Creating #{user['drinks'].count} ingredients"
+
+user['drinks'].each do |item|
+  Ingredient.create!(name: item['strIngredient1'])
+  puts item['strIngredient1']
+end
+puts "finished"
