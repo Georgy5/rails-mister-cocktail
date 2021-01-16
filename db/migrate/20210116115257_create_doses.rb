82f35b1c2c93c444c61554d7cbdc0409b4ec87ad
Karl Keller <kgk86@hotmail.com>
2021-01-16 13:04:59 +0100

    Migration files updated with null: false

--- db/migrate/20210116115257_create_doses.rb
+++ db/migrate/20210116115257_create_doses.rb
@@ -1,7 +1,7 @@
 class CreateDoses < ActiveRecord::Migration[6.0]
   def change
     create_table :doses do |t|
-      t.string :description
+      t.string :description, null: false
       t.references :cocktail, null: false, foreign_key: true
       t.references :ingredient, null: false, foreign_key: true
 
