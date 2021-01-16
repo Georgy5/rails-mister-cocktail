f45ca0875ba78108fff48f49562040d51cf4c1e3
Karl Keller <kgk86@hotmail.com>
2021-01-16 15:39:09 +0100

    Remove unnecessary routes created by rails g controller cocktails index show

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'cocktails/index'
-  get 'cocktails/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/cocktails', to: 'cocktails#index'
   get '/cocktails/:id', to: 'cocktails#show'
