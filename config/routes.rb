4e2f72f338402a2554ddd981d953922b3a1fea0d
Karl Keller <kgk86@hotmail.com>
2021-01-19 20:14:51 +0100

    Nested routes for new doses

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'doses/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'cocktails#index'
-  get '/cocktails/:id', to: 'cocktails#show', as: 'cocktail'
-  get '/cocktails/new', to: 'cocktails#new'
-  post '/cocktails', to: 'cocktails#create'
+  root to: "cocktails#index"
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :doses, only: [:new, :create]
+  end
+  resources :doses, only: [:destroy]
 end
