dc743e8ab19ea8d2e1ef01b3b1fc29e3585acd7c
Karl Keller <kgk86@hotmail.com>
2021-01-19 18:16:39 +0100

    rails g controller doses new

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'doses/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'cocktails#index'
   get '/cocktails/:id', to: 'cocktails#show', as: 'cocktail'
